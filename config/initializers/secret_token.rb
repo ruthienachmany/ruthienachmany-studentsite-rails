984a72072d0ce7ed998580c1cb6e3b23f1fcc673
Ruthie Nachmany <ruthienachmany@gmail.com>
2013-07-07 09:20:14 -0400

    updated gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RuthienachmanyStudentsiteRails::Application.config.secret_key_base = '547cc54a50a7515a369ee1c5559ee8228170cf43d83daa83dbd79a9128b0c4f07bdbd7489eb456e99ad1679bc739aea09dd228224703117e240127f2f6fdb7d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RuthienachmanyStudentsiteRails::Application.config.session_store :cookie_store, key: '_ruthienachmany-studentsite-rails_session'
